57064f086d445a659bb22cbf7c14b70b12638a06
Sérgio Ricardo <working7.sergio@gmail.com>
2025-07-27 01:02:03 -0300

    Update Exercício_1.v1.py

--- Exercícios/Exercício_1.v1.py
+++ Exercícios/Exercício_1.v1.py
@@ -1,5 +1,5 @@
-#Declarando as variáveis
 
+#Declarando as variáveis
 nome_produto = input("Coloque o nome do produto:")
 quantidade_estoque = input("Coloque a quantidade em estoque:")
 quantidade_estoque = float(quantidade_estoque)
@@ -9,9 +9,7 @@ preco_venda = input("coloque o preço de venda:")
 preco_venda=float(preco_venda)
 
 #calculando o lucro por unidade
-
 lucro_unidade = preco_venda-preco_compra
-
 lucro_total = lucro_unidade*quantidade_estoque
 
 print("lucro por unidade de", nome_produto, ":", lucro_unidade )
