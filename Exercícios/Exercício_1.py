471703abd91c76da5da297043739e48ae65ac3d3
Sérgio Ricardo <working7.sergio@gmail.com>
2025-07-26 23:47:38 -0300

    Update Excercício_1

--- Exercícios/Exercício_1.py
+++ Exercícios/Exercício_1.py
@@ -1,5 +1,19 @@
-<<<<<<< HEAD
-'''Criar variáveis para representar: Nome de um produto; Quantidade em estoque; Preço de compra; Preço de venda. aí, depois calcular e exibir o lucro por unidade e calcular e exibir o lucro total'''
-=======
+
 #Criar variáveis para representar: Nome de um produto; Quantidade em estoque; Preço de compra; Preço de venda.
->>>>>>> b9569809e7c7bdfc2c4122695e58ff736d249ea0
+
+#Declarando variáveis
+nome_produto = "morango"
+quantidade_estoque = 250
+preco_compra = 0.75
+preco_de_venda = 2.50
+
+#calculado o lucro por unidade e lucro total
+lucro_unidade = preco_de_venda-preco_compra
+lucro_total = quantidade_estoque*preco_de_venda
+
+print("Lucro por unidade de", nome_produto, "é", lucro_unidade)
+print("lucro total vendendo os 250 morangos é:", lucro_total)
+
+
+
+
