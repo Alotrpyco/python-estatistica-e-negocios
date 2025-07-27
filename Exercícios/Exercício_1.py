9bf6c933a7add1e1571a39f92843754aad8a3e3c
Sérgio Ricardo <working7.sergio@gmail.com>
2025-07-26 23:21:45 -0300

    Update

---
+++ Exercícios/Exercício_1.py
@@ -0,0 +1,1 @@
+# Criar variáveis para representar: Nome de um produto; Quantidade em estoque; Preço de compra; Preço de venda. aí, depois calcular e exibir o lucro por unidade e calcular e exibir o lucro total.
