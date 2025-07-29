0e4940448c45681b15bcd4987c76737027d64d2c
Sérgio Ricardo Vieira Torres Silva <working7.sergio@gmail.com>
2025-07-29 15:12:04 -0300

    Create 3.Exercício.py
    Update

---
+++ Exercícios/3.Exercício.py
@@ -0,0 +1,24 @@
+# Declarando variáveis
+produto1 = 'creatina'
+preço_de_custo_produto1 = 25
+preço_de_venda_produto1 = 80
+quantidade_vendida_produto1 = 100
+produto2 = 'whey'
+preço_de_custo_produto2 = 75
+preço_de_venda_produto2 = 85
+quantidade_vendida_produto2 = 150
+produto3 = 'bcaa'
+preço_de_custo_produto3 = 50
+preço_de_venda_produto3 = 76
+quantidade_vendida_produto3 = 200
+custo_fixo_mensal = 2000
+
+
+#1.Calcular o lucro por unidade de cada produto
+lucro_por_unidade_produto1 = preço_de_venda_produto1 - preço_de_custo_produto1
+lucro_por_unidade_produto2 = preço_de_venda_produto2 - preço_de_custo_produto2
+lucro_por_unidade_produto3 = preço_de_venda_produto3 - preço_de_custo_produto3
+
+print("lucro de uma creatina vendida é", lucro_por_unidade_produto1)
+print("lucro de um whey vendida é", lucro_por_unidade_produto2)
+print("lucro de um bcaa vendida é", lucro_por_unidade_produto3)
