73073a7ba565c800687ccea115bbf71bc09e7fe6
Sérgio Ricardo Vieira Torres Silva <working7.sergio@gmail.com>
2025-07-31 11:35:29 -0300

    Update 3.Exercício.py

--- Exercícios/3.Exercício.py
+++ Exercícios/3.Exercício.py
@@ -20,11 +20,9 @@ custo_fixo_mensal = 2000
 lucro_por_unidade_produto1 = preço_de_venda_produto1 - preço_de_custo_produto1
 lucro_por_unidade_produto2 = preço_de_venda_produto2 - preço_de_custo_produto2
 lucro_por_unidade_produto3 = preço_de_venda_produto3 - preço_de_custo_produto3
-
 print("lucro de uma creatina vendida é", lucro_por_unidade_produto1)
 print("lucro de um whey vendida é", lucro_por_unidade_produto2)
 print("lucro de um bcaa vendida é", lucro_por_unidade_produto3)
-
 
 #2.Calcular o lucro total de cada produto (lucro por unidade × quantidade)
 lucro_total_produto1 = lucro_por_unidade_produto1 * quantidade_vendida_produto1
@@ -34,4 +32,6 @@ print("lucro total obtido com a venda da creatina é",lucro_total_produto1)
 print("lucro total obtido com a venda do whey é", lucro_total_produto2)
 print("lucro total obtido com a venda do bcaa é", lucro_total_produto3)
 
-
+#3.Somar os lucros e subtrair o custo fixo para obter o lucro líquido mensal
+Lucro_líquido_mensal = (lucro_total_produto1 + lucro_total_produto2 + lucro_total_produto3) - custo_fixo_mensal
+print("O lucro obtido no final do mês com a venda dos produtos é", Lucro_líquido_mensal)
