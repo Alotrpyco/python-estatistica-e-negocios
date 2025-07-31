67734e1152c7b82315fb3accd094fd17206f518d
Sérgio Ricardo Vieira Torres Silva <working7.sergio@gmail.com>
2025-07-31 10:52:31 -0300

    Update 3.Exercício.py

--- Exercícios/3.Exercício.py
+++ Exercícios/3.Exercício.py
@@ -24,3 +24,11 @@ lucro_por_unidade_produto3 = preço_de_venda_produto3 - preço_de_custo_produto3
 print("lucro de uma creatina vendida é", lucro_por_unidade_produto1)
 print("lucro de um whey vendida é", lucro_por_unidade_produto2)
 print("lucro de um bcaa vendida é", lucro_por_unidade_produto3)
+
+#2.Calcular o lucro total de cada produto (lucro por unidade × quantidade)
+lucro_total_produto1 = lucro_por_unidade_produto1 * quantidade_vendida_produto1
+lucro_total_produto2 = lucro_por_unidade_produto2 * quantidade_vendida_produto2
+lucro_total_produto3 = lucro_por_unidade_produto3 * quantidade_vendida_produto3
+print("lucro total obtido com a venda da creatina é",lucro_total_produto1)
+print("lucro total obtido com a venda do whey é", lucro_total_produto2)
+print("lucro total obtido com a venda do bcaa é", lucro_total_produto3)
